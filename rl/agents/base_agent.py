3e3a22d03f63717a855b991308bedf413e23f2c2
TheoLvs <theo.alves.da.costa@gmail.com>
2017-09-01 18:01:13 +0200

    Create the base agent

---
+++ rl/agents/base_agent.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+
+"""--------------------------------------------------------------------
+REINFORCEMENT LEARNING
+
+Started on the 25/08/2017
+
+theo.alves.da.costa@gmail.com
+https://github.com/theolvs
+------------------------------------------------------------------------
+"""
+
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np 
+import sys
+import random
+import time
+import random
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+
+
+
+
+
+class Agent(object):
+    def __init__(self):
+        pass
+
+
+    def expand_state_vector(self,state):
+        if len(state.shape) == 1:
+            return np.expand_dims(state,axis = 0)
+        else:
+            return state
+
+
+
+    def remember(self,*args):
+        self.memory.save(args)
+
+
+
+
+
