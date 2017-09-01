da1f88f6d555971f67db232a5b9b49f19f073d81
TheoLvs <theo.alves.da.costa@gmail.com>
2017-09-01 18:02:32 +0200

    create the utils class to plot the running rewards

---
+++ rl/utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+
+"""--------------------------------------------------------------------
+REINFORCEMENT LEARNING
+
+Started on the 25/08/2017
+
+theo.alves.da.costa@gmail.com
+https://github.com/theolvs
+------------------------------------------------------------------------
+"""
+
+
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np 
+import sys
+import random
+import time
+import random
+import numpy as np
+
+
+
+
+def plot_average_running_rewards(rewards):
+    average_running_rewards = np.cumsum(rewards)/np.array(range(1,len(rewards)+1))
+    plt.figure(figsize = (15,4))
+    plt.plot(average_running_rewards)
+    plt.show()
+
+
+
