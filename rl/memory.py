c8e419bbd6d56c73902926589e4c28c9a5fc5ad0
TheoLvs <theo.alves.da.costa@gmail.com>
2017-09-01 18:02:20 +0200

    create the memory class

--- rl/memory.py
+++ rl/memory.py
@@ -13,42 +13,21 @@ https://github.com/theolvs
 """
 
 
-import os
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np 
-import sys
-import random
-import time
-from tqdm import tqdm
 
+from collections import deque
 
 
 
 
 class Memory(object):
-    def __init__(self):
-        self.inputs = None
-        self.targets = None
-        self.length = 0
+    def __init__(self,max_memory = 2000):
+        self.cache = deque(maxlen=max_memory)
     
-    def cache(self,X,y):
-        self.inputs = np.vstack([self.inputs,X]) if self.inputs is not None else X
-        self.targets = np.vstack([self.targets,y]) if self.targets is not None else y
-        self.length = len(self.inputs)
-        
-        
-    def sample(self,batch_size = 32):
-        if self.length > batch_size:
-            selection = np.random.choice(range(self.length),batch_size,replace = False)
-            inputs = self.inputs[selection,:]
-            targets = self.targets[selection,:]
-            return inputs,targets
-        else:
-            return self.inputs,self.targets
-        
-        
+    def save(self,args):
+        self.cache.append(args)
+
     def empty_cache(self):
         self.__init__()
-        
-        
+
+
+
