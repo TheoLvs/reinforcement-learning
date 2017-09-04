dfb9167ad046660bbb0c9884be00402c776e5df8
TheoLvs <theo.alves.da.costa@gmail.com>
2017-09-04 12:20:33 +0200

    update cooling center app

--- 2. Data Center Cooling/app.py
+++ 2. Data Center Cooling/app.py
@@ -20,6 +20,7 @@ https://github.com/theolvs
 
 # USUAL
 import os
+import numpy as np
 
 
 # DASH IMPORT
@@ -34,7 +35,12 @@ import sys
 sys.path.append("C:/git/reinforcement-learning/")
 
 
+class Clicks(object):
+    def __init__(self):
+        self.count = 0
 
+reset_clicks = Clicks()
+train_clicks = Clicks()
 
 #--------------------------------------------------------------------------------
 from rl.env.data_center_cooling import DataCenterCooling
@@ -43,6 +49,64 @@ from rl.agents.dqn_agent import DQNAgent
 
 
 env = DataCenterCooling()
+
+def run_episode(env,agent,max_step = 100,verbose = 1):
+    s = env.reset()
+    
+    episode_reward = 0
+    
+    i = 0
+    while i < max_step:
+        
+        # Choose an action
+        a = agent.act(s)
+        
+        # Take the action, and get the reward from environment
+        s_next,r,done = env.step(a)
+        
+        if verbose: print(s_next,r,done)
+        
+        # Update our knowledge in the Q-table
+        agent.train(s,a,r,s_next)
+        
+        # Update the caches
+        episode_reward += r
+        s = s_next
+        
+        # If the episode is terminated
+        i += 1
+        if done:
+            break
+            
+    return env,agent,episode_reward
+
+
+
+
+def run_n_episodes(env,n_episodes = 2000,lr = 0.8,gamma = 0.95):
+    
+    # Initialize the agent
+    states_size = len(env.observation_space)
+    actions_size = len(env.action_space)
+
+    agent = QAgent(states_size,actions_size,lr = lr,gamma = gamma)
+    
+    # Store the rewards
+    rewards = []
+    
+    # Experience replay
+    for i in tqdm(range(n_episodes)):
+        
+        # Run the episode
+        env,agent,episode_reward = run_episode(env,agent,verbose = 0)
+        rewards.append(episode_reward)
+        
+    
+    # Plot rewards
+    utils.plot_average_running_rewards(rewards)
+        
+    return env,agent,rewards
+        
 
 
 
@@ -69,7 +133,6 @@ container_style = {
 
 AGENTS = [{"label":x,"value":x} for x in ["Q Agent","Deep-Q-Network Agent","Policy Gradient Agent"]]
 
-
 #---------------------------------------------------------------------------------
 # LAYOUT
 app.layout = html.Div(children=[
@@ -81,13 +144,32 @@ app.layout = html.Div(children=[
     # HEADER FIRST CONTAINER
     html.Div([
         html.H2("Data Center Cooling",style = {'color': "rgba(117, 117, 117, 0.95)",**style}),
-        dcc.Dropdown(id = "input-agent",options = AGENTS,value = "Q Agent",multi = False),
-        html.Br(),
-        dcc.Slider(min=100,max=2500,step=50,value=500,id = "n-episodes"),
-        html.Br(),
-        dcc.Slider(min=100,max=2500,step=50,value=500,id = "gamma"),
-        html.Br(),
-        html.Button("Train",id = "training",style = style),
+
+        html.Div([
+            html.H4("Environment",style = {'color': "rgba(117, 117, 117, 0.95)",**style}),
+            html.P("Cooling levels",id = "cooling"),
+            dcc.Slider(min=10,max=100,step=10,value=10,id = "levels-cooling"),
+            html.P("Cost factor",id = "cost-factor"),
+            dcc.Slider(min=1,max=10,step=1,value=5,id = "levels-cost-factor"),
+            html.P("Risk factor",id = "risk-factor"),
+            dcc.Slider(min=1.0,max=2.5,step=0.1,value=1.6,id = "levels-risk-factor"),    
+            html.Button("Reset",id = "reset-env",style = style,n_clicks = 0),
+        ],style = {"height":"50%"}),
+
+
+        html.Div([
+            html.H4("Agent",style = {'color': "rgba(117, 117, 117, 0.95)",**style}),
+            dcc.Dropdown(id = "input-agent",options = AGENTS,value = "Q Agent",multi = False),
+            html.Br(),
+            dcc.Slider(min=100,max=2500,step=50,value=500,id = "n-episodes"),
+            html.Br(),
+            dcc.Slider(min=100,max=2500,step=50,value=500,id = "gamma"),
+            html.Br(),
+            html.Button("Train",id = "training",style = style,n_clicks = 0),
+        ],style = {"height":"50%"}),
+
+
+
     ],style={**style,**container_style,'width': '20%',"height":"800px", 'float' : 'left', 'display': 'inline'}, className="container"),
 
 
@@ -96,8 +178,7 @@ app.layout = html.Div(children=[
     # ANALYTICS CONTAINER
     html.Div([
 
-        dcc.Graph(id='states',animate = False,figure = env.render_states_plotly()),
-        dcc.Graph(id='rewards',animate = False,figure = env.render_rewards_plotly()),
+        dcc.Graph(id='render',animate = False,figure = env.render(with_plotly = True),style = {"height":"100%"}),
 
 
     ],style={**style,**container_style,'width': '55%',"height":"800px", 'float' : 'right', 'display': 'inline'}, className="container"),
@@ -112,6 +193,48 @@ app.layout = html.Div(children=[
 # CALLBACKS
 
 
+
+# Callback to stop the streaming
+@app.callback(
+    Output("render","figure"),
+    [Input('reset-env','n_clicks'),Input('levels-cost-factor','value'),Input('levels-risk-factor','value')],
+    state = [State('levels-cooling','value')]
+
+    )
+def render(click_reset,cost_factor,risk_factor,levels_cooling):
+    if click_reset > reset_clicks.count:
+        reset_clicks.count = click_reset
+        env.__init__(levels_cooling = levels_cooling,risk_factor = risk_factor,cost_factor = cost_factor)
+    else:
+        env.risk_factor = risk_factor
+        env.cost_factor = cost_factor
+
+
+    return env.render(with_plotly = True)
+
+
+
+@app.callback(
+    Output("cooling","children"),
+    [Input('levels-cooling','value')])
+def update_cooling(value):
+    return "Cooling levels : {}".format(value)
+
+
+
+@app.callback(
+    Output("risk-factor","children"),
+    [Input('levels-risk-factor','value')])
+def update_risk(value):
+    return "Risk factor : {}".format(value)
+
+
+
+@app.callback(
+    Output("cost-factor","children"),
+    [Input('levels-cost-factor','value')])
+def update_cost(value):
+    return "Cost factor : {}".format(value)
 
 
 
