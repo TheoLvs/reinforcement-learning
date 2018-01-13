9ab47c495e6b68c0024c06a1686692080b06682a
TheoLvs <theo.alves.da.costa@gmail.com>
2018-01-13 13:04:47 +0100

    genetic logreg framework

--- 4. Chrome Dino/experiments.py
+++ 4. Chrome Dino/experiments.py
@@ -19,7 +19,7 @@ import numpy as np
 import sys
 import time
 from tqdm import tqdm
-
+import itertools
 
 
 
@@ -186,12 +186,14 @@ import torch.nn.functional as F
 
 
 class LogReg(torch.nn.Module):
-    def __init__(self, n_feature,n_output = 1):
+    def __init__(self, n_feature,n_output = 1,alpha = 10e-1):
+        self.alpha = alpha
         self.args = n_feature,n_output
         super(LogReg, self).__init__()
         self.out = torch.nn.Linear(n_feature,n_output,bias = False)   # output layer
 
     def forward(self, x):
+        x = Variable(torch.FloatTensor(x))
         x = F.sigmoid(self.out(x))
         return x
 
@@ -202,10 +204,18 @@ class LogReg(torch.nn.Module):
         return new
 
 
-    def mutate(self):
+    def mutate(self,generation):
         out = self.out.weight.data.numpy()
-        noise_out = 10e-2 * np.random.randn(*out.shape)
+        noise_out = self.alpha * np.random.randn(*out.shape)
         self.out.weight.data = torch.FloatTensor(self.out.weight.data.numpy() + noise_out)
+
+
+    def evaluate(self,x,y):
+        pred = self.forward(x).data.numpy()
+        loss_1 = np.sum(np.log(pred + 10e-9)*y.reshape(-1,1))
+        loss_0 = np.sum(np.log(1-pred + 10e-9)*(1-y).reshape(-1,1))
+        return loss_1 + loss_0
+
 
     def plot_coefs(self):
         plt.figure(figsize = (15,4))
@@ -223,8 +233,10 @@ class LogReg(torch.nn.Module):
 
 
 class PopulationLogReg(object):
-    def __init__(self,regs = None,n = 20,**kwargs):
+    def __init__(self,x,y,regs = None,n = 20,top = 0.25,**kwargs):
 
+        self.x = x
+        self.y = y
         self.kwargs = kwargs
 
         if regs is None:
@@ -251,7 +263,7 @@ class PopulationLogReg(object):
 
 
     def evaluate(self):
-        fitnesses = [(i,element.evaluate()) for i,element in enumerate(self)]
+        fitnesses = [(i,element.evaluate(self.x,self.y)) for i,element in enumerate(self)]
         indices,fitnesses = zip(*sorted(fitnesses,key = lambda x : x[1],reverse = True))
         return indices,fitnesses
 
@@ -278,13 +290,62 @@ class PopulationLogReg(object):
 
 
 
-    def mutate(self):
+    def mutate(self,generation):
         for d in self:
-            d.mutate()
+            d.mutate(generation)
 
 
-    def evolve(self,top = 0.25):
-        indices = self.selection(top = top)
-        self.crossover(indices)
-        self.mutate()
+            
+    def evolve(self,top = 0.25,n_generations = 20,last_selection = True):
+        n_fittest = int(top*len(self))
+        offsprings = len(list(itertools.combinations(range(n_fittest),2)))
+        print("- Generations {}".format(len(self)))
+        print("- Fittest : {}".format(n_fittest))
+        print("- Offsprings : {}".format(offsprings))
+
+        all_fitnesses = [self.evaluate()[1]]
+
+        for generation in tqdm(range(n_generations)):
+
+            indices = self.selection(top)
+            self.crossover(indices)
+            self.mutate(generation)
+            
+            indices,fitnesses = self.evaluate()
+            all_fitnesses.append(fitnesses)
+            
+        self._plot_fitnesses(all_fitnesses)
         
+        if last_selection:
+            indices = self.selection(top)
+    
+        return PopulationLogReg(self.x,self.y,regs = self[indices])
+
+    
+    
+    def _plot_fitnesses(self,fitnesses):
+
+        from sklearn.linear_model import LogisticRegression
+        lr = LogisticRegression()
+        lr.fit(self.x,self.y)
+        pred_bench = lr.predict_proba(self.x)
+        loss_bench = np.sum(np.log(pred_bench + 10e-9)*self.y.reshape(-1,1)) + np.sum(np.log(1-pred_bench + 10e-9)*(1-self.y).reshape(-1,1))
+
+        sups = []
+        infs = []
+        means = []
+        for step in fitnesses:
+            sups.append(np.max(step))
+            infs.append(np.min(step))
+            means.append(np.mean(step))
+            
+        plt.figure(figsize=(10,6))
+        plt.plot(means)
+        plt.fill_between(range(len(means)),sups,infs, alpha = 0.2)
+        plt.axhline(loss_bench)
+        plt.xlabel('# Generation')
+        plt.ylabel('Fitness')
+        plt.legend()
+        plt.show()
+
+
